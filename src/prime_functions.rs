12c3e46ed3222339560ac7feaa79c7ac0d01505e
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2016-04-10 23:12:43 +0300

    two gcd functions

--- src/prime_functions.rs
+++ src/prime_functions.rs
@@ -1,5 +1,7 @@
 use std::vec::Vec;
 use std::iter::repeat;
+use std::cmp::max;
+use std::cmp::min;
 
 pub fn least_divisors_up_to(n : usize) -> (Vec<usize>, Vec<usize>) {
     let mut least_factor = Vec::with_capacity(n+1);
@@ -23,6 +25,30 @@ pub fn least_divisors_up_to(n : usize) -> (Vec<usize>, Vec<usize>) {
 
     }
     (least_factor, primes)
+}
+
+pub fn gcd_recursive(x: i64, y: i64) -> i64 {
+    let (a, b) = (min(x,y), max(x,y));
+    if a == 0 {
+        return 0;
+    }
+    match b % a {
+        0 => a,
+        _ => gcd_recursive(b%a, a),
+    }
+}
+
+pub fn gcd_imperative(x: i64, y: i64) -> i64 {
+    let (mut a, mut b) = (min(x,y), max(x,y));
+    if a == 0 {
+        return 0;
+    }
+    while b % a != 0 {
+        let temp = b % a;
+        b = a;
+        a = temp;
+    }
+    a
 }
 
 
