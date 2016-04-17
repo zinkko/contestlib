9a93c5c0e60e242b85505d674fa4ab0499754664
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2016-04-17 23:30:38 +0300

    added tests to gcd

--- src/math_functions.rs
+++ src/math_functions.rs
@@ -12,7 +12,7 @@ pub fn gcd_recursive(x: i64, y: i64) -> i64 {
     }
 }
 
-pub fn gcd_imperative(x: i64, y: i64) -> i64 {
+pub fn gcd(x: i64, y: i64) -> i64 {
     let (mut a, mut b) = (min(x,y), max(x,y));
     if a == 0 {
         return 0;
@@ -28,8 +28,30 @@ pub fn gcd_imperative(x: i64, y: i64) -> i64 {
 #[cfg(test)]
 mod test {
 
-    #[test]
-    fn it_works() {
+    use super::*;
 
+    #[test]
+    fn gcd_small_coprime() {
+        assert_eq!(gcd(54,35), 1);
+    }
+
+    #[test]
+    fn gcd_small_not_coprime() {
+        assert_eq!(gcd(105, 30), 15);
+    }
+
+    #[test]
+    fn gcd_large_coprime() {
+        let a = 862192863;
+        let b = 3251705600;
+        assert_eq!(gcd(a,b), 1)
+    }
+
+    #[test]
+    fn gcd_large_not_coprime() {
+        let a = 204857452800;
+        let b = 8045539896240;
+        let g = 942480;
+        assert_eq!(gcd(a,b), g);
     }
 }
